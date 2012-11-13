988f835f5e97a81f1b3c62c1f405d2a467f25a7a
sillypog <pete@sillypog.com>
2012-11-13 11:18:52 -0800

    - Changed wrapping of functions to make it clearer that the whole IIEF is an expression. - Moved variable declarations to top of function. - Indentation fixes.

--- templateLoader/plugin/jquery.templateLoader.js
+++ templateLoader/plugin/jquery.templateLoader.js
@@ -1,21 +1,23 @@
 (function (factory) {
-if (typeof define === 'function' && define.amd) {
-	console.log('AMD');	// AMD. Register as an anonymous module.
-    define(['jquery'], factory);
-} else {
-	console.log('Browser globals'); // Browser globals
-    factory(jQuery);
-}
-})(function ($) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['jquery'], factory);
+	} else {
+		// Browser globals
+		factory(jQuery);
+	}
+}(function ($) {
 	$.extend({
 		templateLoader: function(templates, ns){
-							for (var i = 0, l = templates.length; i < l; i++){
-								var t = templates[i];
-									  
-								// Create the hook on the namespace object so that the template can be referenced easily
-								ns[t.toUpperCase()] = t;
-								$.template(t, $('#'+t).remove().html());
-							}
-						}
+			var i = 0,
+				l = templates.length;
+			for (i; i < l; i++){
+				var t = templates[i];
+					  
+				// Create the hook on the namespace object so that the template can be referenced easily
+				ns[t.toUpperCase()] = t;
+				$.template(t, $('#'+t).remove().html());
+			}
+		}
 	});		// Would be called as $.templateLoader([template],namespace);
-});
+}));
